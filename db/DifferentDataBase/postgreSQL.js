63f87d09aff1303ed4812257f58470688cfc89ad
Hongwei <qhwatdmm@hotmail.com>
2019-08-08 14:04:44 -0400

    update and delete some useless files

--- db/DifferentDataBase/postgreSQL.js
+++
@@ -1,9 +0,0 @@
-// const { Client } = require("pg");
-// const config = require("../config");
-
-// const client = new Client(config);
-
-// client.connect()
-// .then(()=> console.log('pg connected successfully'))
-// .catch(()=> console.log('pg faill to connecte!'))
-// .finally(()=> client.end())
