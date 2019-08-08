6dcb46b3a279d126bfad588a3581203a81dee7bb
Hongwei <qhwatdmm@hotmail.com>
2019-08-08 16:19:02 -0400

    add load testing code

--- reviewsAPI/server.js
+++ reviewsAPI/server.js
@@ -12,6 +12,15 @@ app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "./app")));
 app.use("/reviews", Routes);
 
+//load testing
+app.get("/loaderio-79770166efad79bbcbe0c864f8722f52", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.post("/loaderio-79770166efad79bbcbe0c864f8722f52", (req, res) => {
+  res.sendStatus(201);
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
