ed241276000a8042e9a0fcde476516581f3fa05c
Hongwei <qhwatdmm@hotmail.com>
2019-08-09 11:24:01 -0400

    try run without docker

--- reviewsAPI/server.js
+++ reviewsAPI/server.js
@@ -12,16 +12,7 @@ app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "./app")));
 app.use("/reviews", Routes);
 
-//load testing
-app.get("/loaderio-79770166efad79bbcbe0c864f8722f52", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.post("/loaderio-79770166efad79bbcbe0c864f8722f52", (req, res) => {
-  res.sendStatus(201);
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`listening on PORT ${PORT}`);
